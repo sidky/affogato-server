3942e6f63723520100d5ca3ddf364625458ff154
sidky <iamsidky@gmail.com>
2018-01-01 18:40:26 -0800

    Test firebase

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 'use strict';
 
+const firebaseAdmin = require("firebase-admin");
 const Hapi = require('hapi');
+//const hapiAuthFirebase = require('hapi-auth-firebase');
+
+//var serviceAccount = require(process.env.FIREBASE_SERVICE);
+
+// firebaseAdmin.initializeApp({
+//     credential: admin.credential.cert(serviceAccount),
+//     databaseURL: "https://affogato-190622.firebaseio.com/"
+//   });
 
 // Create a server with a host and port
 const server = Hapi.server({ 
     port: process.env.PORT || 8000 
 });
 
-// Add the route
+server.ext('onRequest', (request, h) => {
+    console.log('called: ' + request);
+    return h.continue;
+});
+
 server.route({
     method: 'GET',
     path:'/hello', 
@@ -17,18 +30,25 @@ server.route({
     }
 });
 
-// Start the server
-async function start() {
+server.route({
+    method: 'GET',
+    path: '/api/foo',
+    handler: function (request, h) {
+        return { 'foo': 1, 'bar': 'baz'};
+    }
+});
 
+async function start() {
     try {
         await server.start();
-    }
-    catch (err) {
+    } catch (err) {
         console.log(err);
         process.exit(1);
     }
 
-    console.log('Server running at:', server.info.uri);
-};
+    console.log("" + process.env.FIREBASE_SERVICE);
+
+    console.log('Server running at: ' + server.info.uri);
+}
 
 start();
