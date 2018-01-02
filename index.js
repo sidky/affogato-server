edc5f7f2143ff68d0a99535424edc38f9bf485fb
sidky <iamsidky@gmail.com>
2018-01-01 17:47:13 -0800

    Initial commit with hapi

--- index.js
+++ index.js
@@ -1,8 +1,35 @@
-var express = require('express');
-var app = express();
+'use strict';
 
-const PORT = process.env.PORT || 8080;
+const Hapi = require('hapi');
 
-app.get('/', function(req, res) {
-    res.send('Hello, World!');
-}).listen(PORT,() => console.log(`Listening on ${ PORT }`));
+// Create a server with a host and port
+const server = Hapi.server({ 
+    host: 'localhost', 
+    port: process.env.PORT || 8000 
+});
+
+// Add the route
+server.route({
+    method: 'GET',
+    path:'/hello', 
+    handler: function (request, h) {
+
+        return 'hello world';
+    }
+});
+
+// Start the server
+async function start() {
+
+    try {
+        await server.start();
+    }
+    catch (err) {
+        console.log(err);
+        process.exit(1);
+    }
+
+    console.log('Server running at:', server.info.uri);
+};
+
+start();
